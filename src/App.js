c1aa0a885addced7ea6fc3c80432788696459aac
Aleksei Perepelkin <catdog50rus@gmail.com>
2021-06-19 18:47:14 +0300

    Add sidebarList component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import SidebarList from './components/SidebarList';
+import listSvg from './asserts/img/list.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <div className="todo__sidebar">
+        <SidebarList items = {
+          [
+            {
+              icon: <img src={listSvg} alt="list_item" />,
+              name: 'Все задачи',
+              active: true
+            }
+          ]} />
+          <SidebarList items = {
+          [
+            {
+              color: '#42B883',
+              name: 'Новая задача 1',
+              active: false
+            },
+            {
+              color: '64C4ED',
+              name: 'Новая задача 2',
+              active: false
+            },
+            {
+              color: '64C4ED',
+              name: 'Новая задача 2',
+              active: false
+            }
+          ]} />
+      </div>
+      <div className="todo__tasks">
+        tasks
+      </div>
     </div>
   );
 }
